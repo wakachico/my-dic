7be5a7143ef26377f39b63aa933ce92af64ed97e
wakachico <86215584+wakachico@users.noreply.github.com>
2021-08-09 17:59:40 +0900

    修正３&実装予定の機能追加

--- app/models/word.rb
+++ app/models/word.rb
@@ -109,26 +109,26 @@ class Word < ApplicationRecord
   end
 
   def self.questions(id)
-    ok_id=[] 
+    ok_id=[]                                  #条件を満たしたWord情報のword_idを格納
     5.times do
       count = 0
       while count < 1
-        last = Word.last.id
-        num = rand(0..last)
-        word = Word.find_by('id >= ?', num)
-        if word != nil && word.user_id == id
-          if ok_id.empty? 
-            ok_id.push(word.id)
+        last = Word.last.id                   #wordsテーブルに保存されている最後のword_idを取得
+        num = rand(0..last)                   #ランダムに１件取得するようの"num"変数を用意
+        word = Word.find_by('id >= ?', num)   #num変数以上のword_id情報の中で最初の１件を取得 ※削除機能により抜け番が発生するため
+        if word != nil && word.user_id == id  #word!=nilは保険。word.user_id == id はcurrent_userが登録したword情報かを判別
+          if ok_id.empty?                     #oK_id配列が空だったとき
+            ok_id.push(word.id)                 #条件を満たすword_idをok_idに追加
             count = 1
           end
-          unless ok_id.include?(word.id)
-            ok_id.push(word.id)
+          unless ok_id.include?(word.id)      #oK_id配列に条件を満たすword_idを含まないとき ※テスト問題の一意性を出すため
+            ok_id.push(word.id)                 #条件を満たすword_idをok_idに追加
             count = 1
           end
         end
       end
     end
-    words = Word.where(id: ok_id)
+    words = Word.where(id: ok_id)             #条件をクリアした５件のword_idが格納されたok_idをword_idにもつwordを"words"に格納する
     return words
   end
 end
